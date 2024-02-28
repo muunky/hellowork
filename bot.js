c93a0d7c29458eb7ea528704a4967d38257271a9
muunky <68844500+muunky@users.noreply.github.com>
2024-02-28 17:31:33 +0100

    test

--- bot.js
+++ bot.js
@@ -4,27 +4,27 @@ class EchoBot extends ActivityHandler {
     constructor() {
         super();
 
-        function simulateClick(c, i) {
+        function simulateClick(i,c) {
             const event = new MouseEvent("click", {
               view: window,
               bubbles: true,
               cancelable: true,
             });
-            if ( i != null){
-                const cb = document.getElementById(i);
-            } else if (c != null){
-                const cb = document.getElementsByClassName(c);
-            }
-            const cancelled = !cb.dispatchEvent(event);
-        }
+            if (i != null){
+                var cb = document.getElementById(i);
+              } else if ( c != null){
+                var cb = document.getElementsByClassName(c);
+              }
+            const cancelled = !cb.dispatchEvent(event);    
+        }         
 
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
         this.onMessage(async (context, next) => {
             if (context.activity.text = "hellowork"){
-                Window.open("https://www.hellowork.com/fr-fr/")
+                open("https://www.hellowork.com/fr-fr/")
                 simulateClick("sm:tw-ml-4", null);
                 const replyText = `click reussie`;
-
+                await context.sendActivity(open("https://www.hellowork.com/fr-fr/"));
             } else {
                 const replyText = `Echo: ${ context.activity.text }`;
                 await context.sendActivity(MessageFactory.text(replyText, replyText));
